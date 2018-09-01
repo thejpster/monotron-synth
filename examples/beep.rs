ac7528c600e8b9ae2d994a52862a18130f9f69b1
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2018-09-02 00:03:31 +0100

    Play notes.

--- examples/beep.rs
+++ examples/beep.rs
@@ -2,50 +2,55 @@ extern crate libpulse_binding as pulse;
 extern crate libpulse_simple_binding as psimple;
 extern crate monotron_synth;
 
+use monotron_synth::{Note, Synth};
 use psimple::Simple;
 use pulse::stream::Direction;
-use monotron_synth::{Synth, Note};
 
-const SAMPLE_RATE: u32 = 44_100;
-const BLOCK_SIZE_10MS: usize = SAMPLE_RATE as usize / 100;
+const SAMPLE_RATE: u32 = 80_000_000 / 2112;
+const FRAME_LENGTH_SAMPLES: usize = SAMPLE_RATE as usize / 60;
 
 fn main() -> Result<(), pulse::error::PAErr> {
-	let spec = pulse::sample::Spec {
-	    format: pulse::sample::Format::U8,
-	    channels: 1,
-	    rate: SAMPLE_RATE,
-	};
-	assert!(spec.is_valid());
+    let spec = pulse::sample::Spec {
+        format: pulse::sample::Format::U8,
+        channels: 1,
+        rate: SAMPLE_RATE,
+    };
+    assert!(spec.is_valid());
 
-	let s = Simple::new(
-	    None,                // Use the default server
-	    "MonotronSynthBeep", // Our application's name
-	    Direction::Playback, // We want a playback stream
-	    None,                // Use the default device
-	    "Music",             // Description of our stream
-	    &spec,               // Our sample format
-	    None,                // Use default channel map
-	    None                 // Use default buffering attributes
-	).unwrap();
+    let s = Simple::new(
+        None,                // Use the default server
+        "MonotronSynthBeep", // Our application's name
+        Direction::Playback, // We want a playback stream
+        None,                // Use the default device
+        "Music",             // Description of our stream
+        &spec,               // Our sample format
+        None,                // Use default channel map
+        None,                // Use default buffering attributes
+    ).unwrap();
 
-	let mut synth = Synth::new(SAMPLE_RATE);
+    let mut synth = Synth::new(SAMPLE_RATE);
 
-	let notes = [Note::C4, Note::G4, Note::A4, Note::F4];
-	let mut notes_iter = notes.iter().cycle();
+    let notes = [
+        (Note::C4, 250),
+        (Note::G4, 125),
+        (Note::Rest, 125),
+        (Note::A4, 125),
+        (Note::Rest, 125),
+        (Note::F4, 500),
+    ];
+    let mut notes_iter = notes.iter().cycle();
 
-	for _ in 0..20 {
-		let d = synth.duration(250);
-		synth.play(*notes_iter.next().unwrap(), d);
-		for _ in 0..50 {
-			let mut samples = [0; BLOCK_SIZE_10MS];
-			for sample in samples.iter_mut() {
-				*sample = synth.next().into();
-			}
-			s.write(&samples)?;
-		}
-	}
-
-	s.drain()?;
-
-	Ok(())
+    loop {
+        let (note, duration_ms) = *notes_iter.next().unwrap();
+        let duration_samples = synth.duration_ms_to_samples(duration_ms);
+        synth.play(note, duration_samples);
+        while synth.is_playing() {
+            let mut samples = [0; FRAME_LENGTH_SAMPLES];
+            for sample in samples.iter_mut() {
+                *sample = synth.next().into();
+            }
+            s.write(&samples)?;
+        }
+    }
 }
+
